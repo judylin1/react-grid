eb04f22c8f7f02b2f4fc4dc5b76d4354c98e8023
JudyL <judith.s.lin@gmail.com>
2015-12-09 14:59:52 -0600

    col update

--- src/app/components/main.jsx
+++ src/app/components/main.jsx
@@ -11,19 +11,38 @@ const gridInstance = React.createClass({
       return (
         <Grid>
           <Row className="show-grid">
-            <Col xs={12} md={8} lg={8} style={border}>8</Col>
-            <Col xs={6} md={4} lg={4} style={border}>4</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
+            <Col xs={1} sm={1} md={1} lg={1} style={border}>1</Col>
           </Row>
 
           <Row className="show-grid">
-            <Col xs={6} md={4} style={border}>4</Col>
-            <Col xs={6} md={4} style={border}>4</Col>
-            <Col xs={6} md={4} style={border}>4</Col>
+            <Col xs={4} sm={4} md={4} lg={4} style={border}>4</Col>
+            <Col xs={4} sm={4} md={4} lg={4} style={border}>4</Col>
+            <Col xs={4} sm={4} md={4} lg={4} style={border}>4</Col>
           </Row>
 
           <Row className="show-grid">
-            <Col md={6} mdPush={6} style={border}>6</Col>
-            <Col md={6} mdPull={6} style={border}>6</Col>
+            <Col xs={8} sm={8} md={8} lg={8} style={border}>8</Col>
+            <Col xs={4} sm={4} md={4} lg={4} style={border}>4</Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={6} sm={6} md={6} lg={6} style={border}>6</Col>
+            <Col xs={6} sm={6} md={6} lg={6} style={border}>6</Col>
+          </Row>
+
+          <Row className="show-grid">
+            <Col xs={12} sm={12} md={12} lg={12} style={border}>12</Col>
           </Row>
         </Grid>
       )
